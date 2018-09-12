6ef003e51b4485e42d25b967982a60d1e22e0b78
Ritik Garg <42891900+ritikgarg07@users.noreply.github.com>
2018-09-12 21:28:18 +0530

    Update factorial.py
    Updated the function definition to include 0 factorial. Also added an if statement to handle negative integers.

--- factorial.py
+++ factorial.py
@@ -1,6 +1,10 @@
+#this function returns the value of the factorial of the entered integer.
+#Note - If a negative integer is entered, the return value is a string type.
 def factorial(x) :
-  if x==1 :
+  if x == 0 :
     return 1
+  if x < 0:
+    return " not defined! Factorials are only defined for whole numbers, please try again."
   return x*factorial(x-1)
 print ("Hello User")
 y = int(input("Enter the number whose factorial is to be calculated : "))
